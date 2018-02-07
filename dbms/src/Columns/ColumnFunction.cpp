fc944a8cb87c8ba415275a66ac036463f1cbfc60
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2018-02-07 13:28:07 +0300

    removed prerequisites [#CLICKHOUSE-3550]

--- dbms/src/Columns/ColumnFunction.cpp
+++ dbms/src/Columns/ColumnFunction.cpp
@@ -11,12 +11,113 @@ namespace ErrorCodes
 }
 
 ColumnFunction::ColumnFunction(size_t size, FunctionBasePtr function, const ColumnsWithTypeAndName & columns_to_capture)
-        : size(size), function(function)
+        : size_(size), function(function)
 {
     captured_columns.reserve(function->getArgumentTypes().size());
     for (const auto & column : columns_to_capture)
         appendArgument(column);
 
+}
+
+MutableColumnPtr ColumnFunction::cloneResized(size_t size) const
+{
+    ColumnsWithTypeAndName capture = captured_columns;
+    for (auto & column : capture)
+        column.column = column.column->cloneResized(size);
+
+    return ColumnFunction::create(size, function, capture);
+}
+
+MutableColumnPtr ColumnFunction::replicate(const Offsets & offsets) const
+{
+    if (size_ != offsets.size())
+        throw Exception("Size of offsets doesn't match size of column.", ErrorCodes::SIZES_OF_COLUMNS_DOESNT_MATCH);
+
+    ColumnsWithTypeAndName capture = captured_columns;
+    for (auto & column : capture)
+        column.column = column.column->replicate(offsets);
+
+    return ColumnFunction::create(size_, function, capture);
+}
+
+MutableColumnPtr ColumnFunction::cut(size_t start, size_t length) const
+{
+    ColumnsWithTypeAndName capture = captured_columns;
+    for (auto & column : capture)
+        column.column = column.column->cut(start, length);
+
+    return ColumnFunction::create(size_, function, capture);
+}
+
+MutableColumnPtr ColumnFunction::filter(const Filter & filt, ssize_t result_size_hint) const override
+{
+    ColumnsWithTypeAndName capture = captured_columns;
+    for (auto & column : capture)
+        column.column = column.column->filter(filt, result_size_hint);
+
+    return ColumnFunction::create(size_, function, capture);
+}
+
+MutableColumnPtr ColumnFunction::permute(const Permutation & perm, size_t limit) const override
+{
+    ColumnsWithTypeAndName capture = captured_columns;
+    for (auto & column : capture)
+        column.column = column.column->permute(perm, limit);
+
+    return ColumnFunction::create(size_, function, capture);
+}
+
+
+std::vector<MutableColumnPtr> ColumnFunction::scatter(IColumn::ColumnIndex num_columns,
+                                                      const IColumn::Selector & selector) const
+{
+    std::vector<ColumnsWithTypeAndName> captures(num_columns, captured_columns);
+
+    for (size_t capture = 0; capture < captured_columns.size(); ++capture)
+    {
+        auto parts = captured_columns[capture].column->scatter(num_columns, selector);
+        for (IColumn::ColumnIndex part = 0; part < num_columns; ++part)
+            captures[part][capture] = std::move(parts[part]);
+    }
+
+    std::vector<MutableColumnPtr> columns;
+    columns.reserve(num_columns);
+    for (auto & capture : captures)
+    {
+        size_t size__ = capture.front().column->size();
+        columns.emplace_back(ColumnFunction::create(size__, function, std::move(capture)));
+    }
+
+    return columns;
+}
+
+void ColumnFunction::insertDefault()
+{
+    for (auto & column : captured_columns)
+        column.column->insertDefault();
+}
+void ColumnFunction::popBack(size_t n)
+{
+    for (auto & column : captured_columns)
+        column.column->popBack(n);
+}
+
+size_t ColumnFunction::byteSize() const
+{
+    size_t total_size = 0;
+    for (auto & column : captured_columns)
+        total_size += column.column->byteSize();
+
+    return total_size;
+}
+
+size_t ColumnFunction::allocatedBytes() const
+{
+    size_t total_size = 0;
+    for (auto & column : captured_columns)
+        total_size += column.column->allocatedBytes();
+
+    return total_size;
 }
 
 void ColumnFunction::appendArguments(const ColumnsWithTypeAndName & columns)
@@ -40,7 +141,6 @@ void ColumnFunction::appendArgument(const ColumnWithTypeAndName & column)
 
     captured_columns.push_back(column);
 }
-
 
 ColumnWithTypeAndName ColumnFunction::reduce() const
 {
--- dbms/src/Columns/ColumnFunction.h
+++ dbms/src/Columns/ColumnFunction.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <Core/NamesAndTypes.h>
-#include <Columns/IColumnDummy.h>
+#include <Columns/IColumn.h>
 
 class IFunctionBase;
 using FunctionBasePtr = std::shared_ptr<IFunctionBase>;
@@ -14,35 +14,104 @@ namespace DB
 /** A column containing a lambda expression.
   * Behaves like a constant-column. Contains an expression, but not input or output data.
   */
-class ColumnFunction final : public COWPtrHelper<IColumnDummy, ColumnFunction>
+class ColumnFunction final : public COWPtrHelper<IColumn, ColumnFunction>
 {
 private:
-    friend class COWPtrHelper<IColumnDummy, ColumnFunction>;
+    friend class COWPtrHelper<IColumn, ColumnFunction>;
 
 public:
     ColumnFunction(size_t size, FunctionBasePtr function, const ColumnsWithTypeAndName & columns_to_capture);
 
+    const char * getFamilyName() const override { return "Function"; }
+
+    MutableColumnPtr cloneResized(size_t size) const override;
+
+    size_t size() const override { return size_; }
+
+    MutableColumnPtr cut(size_t start, size_t length) const override;
+    MutableColumnPtr replicate(const Offsets & offsets) const override;
+    MutableColumnPtr filter(const Filter & filt, ssize_t result_size_hint) const override;
+    MutableColumnPtr permute(const Permutation & perm, size_t limit) const override;
+    void insertDefault() override;
+    void popBack(size_t n) override;
+    std::vector<MutableColumnPtr> scatter(IColumn::ColumnIndex num_columns,
+                                          const IColumn::Selector & selector) const override;
+
+    void getExtremes(Field &, Field &) const override {}
+
+    size_t byteSize() const override;
+    size_t allocatedBytes() const override;
+
     void appendArguments(const ColumnsWithTypeAndName & columns);
     ColumnWithTypeAndName reduce() const;
 
-    const char * getFamilyName() const override { return "Function"; }
-
-    MutableColumnPtr replicate(const Offsets & offsets) const override
+    Field operator[](size_t) const override
     {
-        if (size != offsets.size())
-            throw Exception("Size of offsets doesn't match size of column.", ErrorCodes::SIZES_OF_COLUMNS_DOESNT_MATCH);
+        throw Exception("Cannot get value from " + getName(), ErrorCodes::NOT_IMPLEMENTED);
+    }
 
-        ColumnsWithTypeAndName replicated_capture;
-        replicated_capture.reserve(captured_columns.size());
+    void get(size_t, Field &) const override
+    {
+        throw Exception("Cannot get value from " + getName(), ErrorCodes::NOT_IMPLEMENTED);
+    }
 
-        for (const auto & column : captured_columns)
-            replicated_capture.push_back({column.column->replicate(offsets), column.type, column.name});
+    void insert(const Field &) override
+    {
+        throw Exception("Cannot insert element into " + getName(), ErrorCodes::NOT_IMPLEMENTED);
+    }
 
-        return ColumnFunction::create(size == 0 ? 0 : offsets.back(), function, replicated_capture);
+    StringRef getDataAt(size_t n) const override
+    {
+        throw Exception("Cannot get value from " + getName(), ErrorCodes::NOT_IMPLEMENTED);
+    }
+
+    void insert(const Field &) override
+    {
+        throw Exception("Cannot get insert into " + getName(), ErrorCodes::NOT_IMPLEMENTED);
+    }
+
+    void insertRangeFrom(const IColumn &, size_t, size_t) override
+    {
+        throw Exception("Cannot insert into " + getName(), ErrorCodes::NOT_IMPLEMENTED);
+    }
+
+    void insertData(const char *, size_t) override
+    {
+        throw Exception("Cannot insert into " + getName(), ErrorCodes::NOT_IMPLEMENTED);
+    }
+
+    StringRef serializeValueIntoArena(size_t, Arena &, char const *&) const override
+    {
+        throw Exception("Cannot serialize from " + getName(), ErrorCodes::NOT_IMPLEMENTED);
+    }
+
+    const char * deserializeAndInsertFromArena(const char *) override
+    {
+        throw Exception("Cannot deserialize to " + getName(), ErrorCodes::NOT_IMPLEMENTED);
+    }
+
+    void updateHashWithValue(size_t, SipHash &) const override
+    {
+        throw Exception("updateHashWithValue is not implemented for " + getName(), ErrorCodes::NOT_IMPLEMENTED);
+    }
+
+    int compareAt(size_t, size_t, const IColumn &, int) const override
+    {
+        throw Exception("compareAt is not implemented for " + getName(), ErrorCodes::NOT_IMPLEMENTED);
+    }
+
+    void getPermutation(bool, size_t, int, Permutation &) const override
+    {
+        throw Exception("getPermutation is not implemented for " + getName(), ErrorCodes::NOT_IMPLEMENTED);
+    }
+
+    void gather(ColumnGathererStream &) override
+    {
+        throw Exception("Method gather is not supported for " + getName(), ErrorCodes::NOT_IMPLEMENTED);
     }
 
 private:
-    size_t size;
+    size_t size_;
     FunctionBasePtr function;
     ColumnsWithTypeAndName captured_columns;
 
